e38745baf913ab78474aae78d87216cc22aaea5e
HackISTP <hyeminpak@gmail.com>
2023-01-25 21:01:20 +0900

    :sparkles:	ADD: socket.io 로 채팅구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,2 +1,53 @@
 //프론트에서 구동되는 코드
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+let roomName = "";
+room.hidden = true;
+const addMessage = (message) => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  console.log(message);
+  li.innerText = message;
+  ul.appendChild(li);
+};
+const handleMessageSubmit = (event) => {
+  event.preventDefault();
+  const input = room.querySelector("#msg input");
+  const msg = input.value;
+  socket.emit("new_Message", input.value, roomName, () =>
+    addMessage(`You ${msg}`)
+  ); //백엔드로 메세지 보내기
+  input.value = "";
+};
+const handleNickNameSubmit = (event) => {
+  event.preventDefault();
+  const input = room.querySelector("#nickname input");
+  socket.emit("nickname", input.value);
+};
+const showRoom = () => {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector("#msg");
+  const nameForm = room.querySelector("#nickname ");
+  msgForm.addEventListener("submit", handleMessageSubmit);
+  nameForm.addEventListener("submit", handleNickNameSubmit);
+};
+const handleRoomSubmit = (event) => {
+  event.preventDefault();
+  const input = document.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
+
+  input.value = "";
+};
+
+form.addEventListener("submit", handleRoomSubmit);
+//백엔드에서 받는
+socket.on("welcome", (user) => addMessage(`${user} 님이 접속하셨습니다.`));
+socket.on("bye", (user) => addMessage(`${user} 님이 나갔습니다.`));
+socket.on("new_Message", addMessage);
--- src/server.js
+++ src/server.js
@@ -15,7 +15,30 @@ const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
 io.on("connection", (socket) => {
-  console.log(socket);
+  socket["nickName"] = "익명";
+  socket.onAny((event) => {
+    console.log(`socket event : ${event}`);
+  });
+  socket.on("enter_room", (roomName, done) => {
+    console.log(socket.id);
+    console.log(socket.rooms);
+    socket.join(roomName);
+    console.log(socket.rooms);
+    done();
+    socket.to(roomName).emit("welcome", socket.nickName); //방안에 있는 모두에게 welcome 을 보낸다.
+  });
+  //프론트에서 받음
+  socket.on("disconnecting", () => {
+    //클라이언트가 서버와 연결이 끊겨지기 전에 실행할수 있게 함
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("bye", socket.nickName);
+    });
+  });
+  socket.on("new_Message", (msg, roomName, done) => {
+    socket.to(roomName).emit("new_Message", `${socket.nickName}: ${msg}`);
+    done();
+  });
+  socket.on("nickname", (nickname) => (socket["nickName"] = nickname));
 });
 
 httpServer.listen(3000, handleListen);
